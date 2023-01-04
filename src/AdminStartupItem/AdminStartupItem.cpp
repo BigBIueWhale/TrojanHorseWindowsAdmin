521f16910cfcc2e4540af57674a0004cd60945ed
Ronen Zyroff <rzyroff@gmail.com>
2023-01-04 05:54:08 -0800

    Trying to use Task Scheduler API because setting shortcut in Startup folder didn't work (if task scheduler doesn't work either, I will try disabling UAC programmatically to make the startup folder method work)

--- src/AdminStartupItem/AdminStartupItem.cpp
+++ src/AdminStartupItem/AdminStartupItem.cpp
@@ -3,102 +3,226 @@
 #include <error_message_macro.hpp>
 #include <Win32ErrorMessage.hpp>
 #include <Windows.h>
-#include <shobjidl_core.h>
+
+// Include Windows libraries required for creating a task
+#include <taskschd.h>
+#include <comdef.h>
+#include <comip.h>
+#include <comdefsp.h>
+#include <comutil.h>
+
 #include <filesystem>
 #include <sstream>
 #include <stdexcept>
 
-// Create IShellLink wrapper RAII class to create shortcut files
-class ShellLink
-{
-private:
-    IShellLink* m_shell_link = nullptr;
-public:
-    ShellLink() {
-        const HRESULT hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&m_shell_link));
-        if (hr != S_OK || m_shell_link == nullptr) {
-            switch (hr) {
-                case REGDB_E_CLASSNOTREG:
-                    throw std::runtime_error{ ERROR_MESSAGE("CoCreateInstance failed with REGDB_E_CLASSNOTREG") };
-                case CLASS_E_NOAGGREGATION:
-                    throw std::runtime_error{ ERROR_MESSAGE("CoCreateInstance failed with CLASS_E_NOAGGREGATION") };
-                case E_NOINTERFACE:
-                    throw std::runtime_error{ ERROR_MESSAGE("CoCreateInstance failed with E_NOINTERFACE") };
-                case E_POINTER:
-                    throw std::runtime_error{ ERROR_MESSAGE("CoCreateInstance failed with E_POINTER") };
-                case E_ACCESSDENIED:
-                    throw std::runtime_error{ ERROR_MESSAGE("CoCreateInstance failed with E_ACCESSDENIED") };
-                default:
-                {
-                    std::ostringstream err_msg;
-                    err_msg << "CoCreateInstance failed with HRESULT: " << std::to_string(hr)
-                        << "Did you remember to call CoInitializeEx?";
-                    throw std::runtime_error{ ERROR_MESSAGE(err_msg.str()) };
-                }
-            }
-        }
-    }
-    IShellLink* getShellLinkHandle() {
-        if (m_shell_link == nullptr) {
-            throw std::runtime_error{ ERROR_MESSAGE("m_shell_link is nullptr") };
-        }
-        return m_shell_link;
-    }
-    ~ShellLink() {
-        if (m_shell_link) {
-            static_cast<void>(m_shell_link->Release());
-        }
-    }
-};
-
-// Create a wrapper RAII class for IPersistFile
-class PersistFile
-{
-private:
-    IPersistFile* m_persist_file = nullptr;
-public:
-    PersistFile(IShellLink* shell_link) {
-        const HRESULT hr = shell_link->QueryInterface(IID_PPV_ARGS(&m_persist_file));
-        if (FAILED(hr)) {
-            throw std::runtime_error{ ERROR_MESSAGE("IShellLinkW::QueryInterface failed") };
-        }
-    }
-    IPersistFile* getPersistFileHandle() {
-        if (m_persist_file == nullptr) {
-            throw std::runtime_error{ ERROR_MESSAGE("m_persist_file is nullptr") };
-        }
-        return m_persist_file;
-    }
-    ~PersistFile() {
-        if (m_persist_file) {
-            static_cast<void>(m_persist_file->Release());
-        }
-    }
-};
-
 void AdminStartupItem::Create(std::wstring executable_path)
 {
-    // Create a shortcut file for the executable in:
-    // C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+    // Schedule a task to run on login of any user.
+    // The task will run with the highest privilages.
+    // The task will run whether the user is logged-in or not (will not store credentials).
+    // Configure the task for Windows 10
+    // Make the task hidden
 
-    // Use the Win32API to create a shortcut of the executable
+    // Create the task
+    ITaskService* pService = nullptr;
+    ITaskFolder* pRootFolder = nullptr;
+    ITaskDefinition* pTask = nullptr;
+    IRegistrationInfo* pRegInfo = nullptr;
+    ITaskSettings* pSettings = nullptr;
+    IPrincipal* pPrincipal = nullptr;
+    IActionCollection* pActionCollection = nullptr;
+    IAction* pAction = nullptr;
+    IExecAction* pExecAction = nullptr;
+    ITriggerCollection* pTriggerCollection = nullptr;
+    ITrigger* pTrigger = nullptr;
+    ILogonTrigger* pLogonTrigger = nullptr;
+    IRegisteredTask* pRegisteredTask = nullptr;
+    HRESULT hr = S_OK;
 
-    const std::filesystem::path executable_path_fs{ executable_path };
-    const std::filesystem::path startup_folder_path = L"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Startup";
+    // Assuming the the COMbase library has been initialized
 
-    const std::filesystem::path shortcut_file_path = startup_folder_path / (executable_path_fs.stem().wstring() + L".lnk");
-
-    // Create a shortcut file for the executable in: shortcut_file_path
-    ShellLink shell_link{};
-    const HRESULT hr = shell_link.getShellLinkHandle()->SetPath(executable_path.c_str());
-    if (FAILED(hr)) {
-        throw std::runtime_error{ ERROR_MESSAGE("IShellLinkW::SetPath failed") };
+    // Get the task service
+    hr = CoCreateInstance(CLSID_TaskScheduler, nullptr, CLSCTX_INPROC_SERVER, IID_ITaskService, reinterpret_cast<void**>(&pService));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to create the task service. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
     }
-    PersistFile persist_file{ shell_link.getShellLinkHandle() };
-    const HRESULT hr2 = persist_file.getPersistFileHandle()->Save(shortcut_file_path.c_str(), TRUE);
-    if (FAILED(hr2)) {
-        throw std::runtime_error{ ERROR_MESSAGE("IPersistFile::Save failed") };
+
+    // Connect to the task service
+    hr = pService->Connect(_variant_t(), _variant_t(), _variant_t(), _variant_t());
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to connect to the task service. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
     }
+
+    // Get the root folder
+    hr = pService->GetFolder(_bstr_t(L"\\"), &pRootFolder);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the root folder. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the task definition
+    hr = pService->NewTask(0, &pTask);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the task definition. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the registration info
+    hr = pTask->get_RegistrationInfo(&pRegInfo);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the registration info. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the registration info
+    hr = pRegInfo->put_Author(_bstr_t(L""));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the registration info. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the task settings
+    hr = pTask->get_Settings(&pSettings);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the task settings. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the task settings
+    hr = pSettings->put_DisallowStartIfOnBatteries(VARIANT_FALSE);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the task settings. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // TODO:
+    // // Hide task
+    // hr = pSettings->put_Hidden(VARIANT_TRUE);
+
+    // Get the principal
+    hr = pTask->get_Principal(&pPrincipal);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the principal. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the principal
+    hr = pPrincipal->put_RunLevel(TASK_RUNLEVEL_HIGHEST);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the principal. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Don't store credentials
+    hr = pPrincipal->put_LogonType(TASK_LOGON_NONE);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the principal. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the action collection
+    hr = pTask->get_Actions(&pActionCollection);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the action collection. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Create the action
+    hr = pActionCollection->Create(TASK_ACTION_EXEC, &pAction);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to create the action. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the exec action
+    hr = pAction->QueryInterface(IID_IExecAction, reinterpret_cast<void**>(&pExecAction));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the exec action. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the exec action to run executable_path
+    hr = pExecAction->put_Path(_bstr_t(executable_path.c_str()));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the exec action. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the trigger collection
+    hr = pTask->get_Triggers(&pTriggerCollection);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the trigger collection. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the trigger to run at login of any user
+    hr = pTriggerCollection->Create(TASK_TRIGGER_LOGON, &pTrigger);
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the trigger. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Get the logon trigger
+    hr = pTrigger->QueryInterface(IID_ILogonTrigger, reinterpret_cast<void**>(&pLogonTrigger));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to get the logon trigger. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Set the logon trigger to run at login of any user
+    hr = pLogonTrigger->put_Id(_bstr_t(L"Trigger1"));
+    if (FAILED(hr))
+    {
+        std::ostringstream oss;
+        oss << "Failed to set the logon trigger. Error code: " << hr;
+        throw std::runtime_error{ ERROR_MESSAGE(oss.str()) };
+    }
+
+    // Release the task
+    pTask->Release();
+    pTask = nullptr;
+
+    // Release the service
+    pService->Release();
+    pService = nullptr;
 }
 
 AdminStartupItem::AdminStartupItem() = default;
